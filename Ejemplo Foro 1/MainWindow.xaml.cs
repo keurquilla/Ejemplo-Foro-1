943a74af39715a3cf2b844be6f5c2e28a718de48
Keurquilla <keurquilla4@gmail.com>
2021-08-24 19:21:12 -0600

    Agregar archivos de proyecto.

---
+++ Ejemplo Foro 1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejemplo_Foro_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string mensaje;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void but1_Click(object sender, RoutedEventArgs e)
+        {
+            mensaje = "Hola Mundo" + " saludos al Foro 1";
+            Label2.Content = mensaje;
+
+            
+        }
+
+        private void label1_Initialized(object sender, EventArgs e)
+        {
+            mensaje = "Programa Funcionando";
+            label1.Content = mensaje;
+        }
+
+        
+    }
+}
